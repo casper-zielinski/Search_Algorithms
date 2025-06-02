e15fa73e89292d3e42115e8b37a4681d1357877c
Zielinski Casper <casper.zielinski.endercasper@gmail.com>
2025-06-02 10:53:13 +0200

    App.JAva Test Cases

--- src/main/java/at/fhj/msd/App.java
+++ src/main/java/at/fhj/msd/App.java
@@ -11,25 +11,6 @@ public class App {
       private static int[] hundredthousand = java.util.stream.IntStream.rangeClosed(1, 100000).toArray();
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        long start = System.currentTimeMillis();
-
-        int[] a = {
-                1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                15, 20, 25, 30, 35, 40, 45, 50, 55, 60,
-                65, 70, 75, 80, 85, 90, 95, 100, 110, 120,
-                130, 140, 150, 160, 170, 180, 190, 200, 220, 240,
-                260, 280, 300, 320, 340, 360, 380, 400, 420, 440
-        };
-
-        int index = SearchAlgorithms.interpolationSearch(a, 340);
-
-        long end = System.currentTimeMillis();
-
-        long duration = end - start;
-
-        System.out.printf("Found Index: %d || Duration: %d", index, duration);
-=======
         Static_analysis static_analysis = new Static_analysis();
         
         System.out.println(static_analysis.TimeMeasureLinearSearch(1, "Best Case", hundred));
@@ -59,10 +40,6 @@ public class App {
         System.out.println(static_analysis.TimeMeasureQuadraticBinarySearch(5000, "Avarage Case", tenthousand));
         System.out.println(static_analysis.TimeMeasureQuadraticBinarySearch(50000, "Avarage Case", hundredthousand));
         System.out.println(static_analysis.TimeMeasureQuadraticBinarySearch(1000000, "Worst Case", hundredthousand));
-
-
-
-
->>>>>>> 847adb7c57e7a8c699ac733ec2b8a719ecda52ed
+        
     }
 }
