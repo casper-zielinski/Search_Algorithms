0355b2a4d0919f770fd78a20bf9408e17f653f4f
Umejr Dzinovic <Umejr.Dzinovic@edu.fh-joanneum.at>
2025-06-02 07:35:32 +0200

    Fixed Integer overflow in proble formula

--- src/main/java/at/fhj/msd/App.java
+++ src/main/java/at/fhj/msd/App.java
@@ -1,5 +1,7 @@
 package at.fhj.msd;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  */
@@ -11,25 +13,6 @@ public class App {
       private static int[] hundredthousand = java.util.stream.IntStream.rangeClosed(1, 100000).toArray();
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        long start = System.currentTimeMillis();
-
-        int[] a = {
-                1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                15, 20, 25, 30, 35, 40, 45, 50, 55, 60,
-                65, 70, 75, 80, 85, 90, 95, 100, 110, 120,
-                130, 140, 150, 160, 170, 180, 190, 200, 220, 240,
-                260, 280, 300, 320, 340, 360, 380, 400, 420, 440
-        };
-
-        int index = SearchAlgorithms.interpolationSearch(a, 340);
-
-        long end = System.currentTimeMillis();
-
-        long duration = end - start;
-
-        System.out.printf("Found Index: %d || Duration: %d", index, duration);
-=======
         Static_analysis static_analysis = new Static_analysis();
         
         System.out.println(static_analysis.TimeMeasureLinearSearch(1, "Best Case", hundred));
@@ -63,6 +46,5 @@ public class App {
 
 
 
->>>>>>> 847adb7c57e7a8c699ac733ec2b8a719ecda52ed
     }
 }
